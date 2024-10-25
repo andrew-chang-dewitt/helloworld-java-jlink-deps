2c33dbf2d56cc98e1730028063209490a4f73b3d
Andrew Chang-DeWitt <11323923+andrew-chang-dewitt@users.noreply.github.com>
2024-10-25 14:51:32 -0500

    Fix ref.

--- src/main/java/org/jreleaser/examples/HelloWorld.java
+++ src/main/java/org/jreleaser/examples/HelloWorld.java
@@ -21,7 +21,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class HelloWorld {
-    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+    private static final Logger LOGGER = LogManager.getLogger(HelloWorld.class);
 
     public static void main(String[] args) {
         String name = "world";
